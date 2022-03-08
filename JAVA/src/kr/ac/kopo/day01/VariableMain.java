a132f3f434d7b8fd09753a309c84c8eab3d2bcf0
sumin-22 <cjstk4_@naver.com>
2022-03-08 15:18:33 +0900

    2

---
+++ JAVA/src/kr/ac/kopo/day01/VariableMain.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.day01;
+
+/* 		기본 자료형
+  		논리형 : boolean
+  		문자형: char
+  		정수형: byte, short, int(*), long
+  		실수형: float, double(*)
+ */
+public class VariableMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
