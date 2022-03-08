d31dc500453520bbc2e891d57c27c4c7c3a4744c
sumin-22 <cjstk4_@naver.com>
2022-03-08 16:51:58 +0900

    d

--- JAVA/src/kr/ac/kopo/day01/VariableMain03.java
+++ JAVA/src/kr/ac/kopo/day01/VariableMain03.java
@@ -4,6 +4,13 @@ public class VariableMain03 {
 
 	public static void main(String[] args) {
 		
+		int a;
+		byte b =10;
+		
+		a = b; //byte가 int보다 크기가 작기 때문에 묵시적 형변환 가능
+		b = (byte)a;
+		
+		
 		
 
 	}
