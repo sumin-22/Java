57d5afa6a0e2b3cccfb6c3bfff756bf6de26a446
sumin-22 <cjstk4_@naver.com>
2022-03-10 15:29:10 +0900

    1

--- JAVA/src/kr/ac/kopo/day02/SwitchMain01.java
+++ JAVA/src/kr/ac/kopo/day02/SwitchMain01.java
@@ -14,9 +14,21 @@ public class SwitchMain01 {
 
 	public static void main(String[] args) {
 		
-		Scanner s = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("1 ~ 3 사이의 정수 입력 : ");
+		int num = sc.nextInt();
+		
+		switch (num) {
+		case 1 :
+			System.out.println("ONE");
+		case 2 :
+			System.out.println("TWO");
+		case 3 :
+			System.out.println("THREE");
+		default :
+			System.out.println("ERROR");
+		}
 		
 		
 
