a79945b0cf152624152beeedb6d26de59370aad7
sumin-22 <cjstk4_@naver.com>
2022-03-10 16:32:32 +0900

    s

---
+++ JAVA/src/kr/ac/kopo/day02/LoopMain01.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.day02;
+
+/* 
+  	반복문을 위한 기본요소 4가지
+  	1. 반복할 문장 (Statement)
+  	2. 시작값(init)
+  	3. 증가/감소 (incre.decre)
+  	4. 종료조건(expr)
+  	
+  	한바퀴 뛸 차례다;
+  	while(!열바퀴를 다 뛰셨나요?) {
+  		운동장 한바퀴 뛴다;
+  		바퀴수 증가;
+  	}
+  	
+  	1. init
+  	while(2.expr) {
+  		3.statement;
+  		4.incre/dere;
+  	}
+  	
+ */
+
+public class LoopMain01 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
