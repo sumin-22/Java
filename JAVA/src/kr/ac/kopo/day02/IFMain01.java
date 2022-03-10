523c0c9bed648b32124f9ce43df2a67687ae4153
sumin-22 <cjstk4_@naver.com>
2022-03-10 14:06:31 +0900

    d

--- JAVA/src/kr/ac/kopo/day02/IFMain01.java
+++ JAVA/src/kr/ac/kopo/day02/IFMain01.java
@@ -6,13 +6,30 @@ package kr.ac.kopo.day02;
  *if(조건) {
  * 		문장;
  * }  
+ * 
+ * 
+ * if(조건){
+ * 		문장1;
+ * } else(조건){
+ * 		문장2;
+ * }
+ * 
+ * 보유하고 있는 책 권수 입력 : 1
+ * 1 book
+ * 
+ * 보유하고 있는 책 권수 입력 : 4
+ * 4 books
  */
 
 public class IFMain01 {
 
 	public static void main(String[] args) {
 		
-
+		int book = 1;
+		
+		System.out.print(book +" book");
+		if(book >=2) {
+			System.out.println("s");
+		}
 	}
-
 }
---
+++ JAVA/src/kr/ac/kopo/day02/IFMain02.java
@@ -0,0 +1,36 @@
+package kr.ac.kopo.day02;
+
+import java.util.Scanner;
+
+/*
+ 	N개의 문장을 표현하기 위해서는 n-1개의 조건식이 필요하다
+ 	
+ 	if(조건1) {
+ 		문장 1;
+ 	} else{                      조건1일 거짓인 것들 중에서 
+ 		if(조건2) {               조건2가 참이면 문장2를, 조건이 거짓이면 문장3을 출력
+ 			문장2;
+ 		} else{
+ 			문장3;
+ 		}
+ 	}
+ 	
+ 	
+ 	출발한 시간을 입력하세요 (ex 7시 30분 : 730) -> 807
+ 	버스를 탑니다
+ */
+
+public class IFMain02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("집에서 출발한 시간을 입력하세요 (7시 30분 : 730) -> ");
+		int time = sc.nextInt();
+		
+		
+
+	}
+
+}
