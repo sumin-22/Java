f7c4012825c572c8292ecc49886132ffa313b1d6
sumin-22 <cjstk4_@naver.com>
2022-03-10 09:56:06 +0900

    ㄴ

---
+++ JAVA/src/kr/ac/kopo/day02/LottoUtil.java
@@ -0,0 +1,25 @@
+package kr.ac.kopo.day02;
+import java.util.Random;
+
+/** 
+ * 로또와 관련된 여러가지 기능클래스
+ * @author 수민
+ *
+ */
+
+public class LottoUtil {
+	
+	/**
+	 * 오늘의 로또 확률을 알려주는 메소드
+	 */
+	
+	public void todayProbability() {
+		
+		Random r = new Random();
+		int p = r.nextInt(101); // 0~100 
+		
+		System.out.println("오늘의 로또 확률은 " + p + "%입니다");
+		
+	}
+
+}
