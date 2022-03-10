2329f3c0453602741a7d78622f5f335e4bab9b26
sumin-22 <cjstk4_@naver.com>
2022-03-10 15:03:57 +0900

    4

--- JAVA/src/kr/ac/kopo/day02/IFMain03.java
+++ JAVA/src/kr/ac/kopo/day02/IFMain03.java
@@ -1,5 +1,7 @@
 package kr.ac.kopo.day02;
 
+import java.util.Scanner;
+
 /*
  	if(조건1) {
  		문장1;
@@ -15,16 +17,35 @@ package kr.ac.kopo.day02;
  				} else {
  					if (조건5) {
  						문장5;
- 					}
- 				
+ 					}		
  	}
+ 	
+ 	
+ 	
+ 		다중if
+ 		if(조건1) {
+ 		} else if (조건2) {
+ 		} else if (조건3) {
+ 		} else {
+ 		}
  */
+
+
 
 public class IFMain03 {
 
 	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
 		
-
+		System.out.print("집에서 출발한 시간을 입력하세요 (7시 30분 : 730) -> ");
+		int time = sc.nextInt();
+		
+		if (time < 800) {
+			System.out.println("걸어서 학교에 간다");
+		} else if (time < 815) {
+			System.out.println("버스를 타고 학교에 간다");
+		} else {
+			System.out.println("택시를 타고 학교에 간다");			
+		}
 	}
-
 }
