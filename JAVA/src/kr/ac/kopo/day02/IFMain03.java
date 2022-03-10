1f0bf0d2d09134cf4b96cf7e9d67ea6971120b22
sumin-22 <cjstk4_@naver.com>
2022-03-10 14:55:49 +0900

    7

--- JAVA/src/kr/ac/kopo/day02/IFMain03.java
+++ JAVA/src/kr/ac/kopo/day02/IFMain03.java
@@ -1,5 +1,25 @@
 package kr.ac.kopo.day02;
 
+/*
+ 	if(조건1) {
+ 		문장1;
+ 	} else {
+ 		if(조건2) {
+ 			문장2;
+ 		} else {
+ 			if(조건3) {
+ 				문장3;
+ 			} else {
+ 				if (조건4) {
+ 					문장4;
+ 				} else {
+ 					if (조건5) {
+ 						문장5;
+ 					}
+ 				
+ 	}
+ */
+
 public class IFMain03 {
 
 	public static void main(String[] args) {
