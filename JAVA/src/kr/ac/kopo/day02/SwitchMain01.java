d4d79ab81b28be98dcf2737a372e836923bab1c6
sumin-22 <cjstk4_@naver.com>
2022-03-10 15:22:51 +0900

    ㄴ

---
+++ JAVA/src/kr/ac/kopo/day02/SwitchMain01.java
@@ -0,0 +1,25 @@
+package kr.ac.kopo.day02;
+
+import java.util.Scanner;
+
+/*
+ 	1 ~ 3 사이의 정수 입력:
+ 	1 입력시 "ONE" 출력
+ 	2 입력시 "TWO" 츨력
+ 	3 입력시 "THREE" 출력
+ 	그 외   "ERROR" 출력
+ */
+
+public class SwitchMain01 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("1 ~ 3 사이의 정수 입력 : ");
+		
+		
+
+	}
+
+}
