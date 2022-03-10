4d7e9e56622ab2f1034336fa04ddf240ac583840
sumin-22 <cjstk4_@naver.com>
2022-03-10 11:56:24 +0900

    commit

--- JAVA/src/kr/ac/kopo/day02/OperationMain02.java
+++ JAVA/src/kr/ac/kopo/day02/OperationMain02.java
@@ -20,6 +20,7 @@ public class OperationMain02 {
 		Scanner sc = new Scanner(System.in); //ctrl + shift + o
 		
 		System.out.print("a 입력 : ");
+		
 		int a = sc.nextInt();
 		
 		System.out.print("b 입력 : ");
