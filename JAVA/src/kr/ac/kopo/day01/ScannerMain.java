c5792a4c849993642e35186268fb3ccfa75fc2f9
sumin-22 <cjstk4_@naver.com>
2022-03-10 12:01:08 +0900

    dd

--- JAVA/src/kr/ac/kopo/day01/ScannerMain.java
+++ JAVA/src/kr/ac/kopo/day01/ScannerMain.java
@@ -3,7 +3,6 @@ package kr.ac.kopo.day01;
 import java.util.Scanner;
 
 public class ScannerMain {
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 		
@@ -25,28 +24,4 @@ public class ScannerMain {
 		
 	}
 
-=======
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("문자를 입력 : ");
-		char ch = sc.nextLine().charAt(0);
-		System.out.println("ch : " + ch);
-		
-//		System.out.print("문자열을 입력 : ");
-//		String st1 = sc.next();
-//		System.out.println("st1 : " + st1);
-		
-		System.out.print("문장을 입력 : ");
-		String st2 = sc.nextLine();
-		System.out.println("st2 : " + st2);
-	
-		System.out.print("정수를 하나 입력하세요 : ");
-		int a = sc.nextInt();
-		System.out.println("a : " + a);
-		
-	}
->>>>>>> refs/remotes/origin/main
 }
