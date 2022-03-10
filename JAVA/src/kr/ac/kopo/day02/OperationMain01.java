586f32784ecfa10c90eba9dcdb8dceb534d0e803
sumin-22 <cjstk4_@naver.com>
2022-03-10 11:32:23 +0900

    ss

--- JAVA/src/kr/ac/kopo/day02/OperationMain01.java
+++ JAVA/src/kr/ac/kopo/day02/OperationMain01.java
@@ -3,8 +3,18 @@ package kr.ac.kopo.day02;
 public class OperationMain01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a = 1, b = 0, c = -1;
+		
+		//boolean bool = ++a > 1 || ++b > 1;
+		// boolean bool = --a > 1 && ++b > 1 || ++c > 0;
+		//boolean bool = a++ > 0 ||b-- >=0 && c++ <0;
+		boolean bool = b-- >=0 && c++ <=0 || a++ >0;
+		
+		System.out.println("bool : " + bool);
+		System.out.println("a : " + a + ", b : " + b + ", c : " + c);
+		
+		
 	}
 
 }
