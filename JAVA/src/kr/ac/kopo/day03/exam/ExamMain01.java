f6ab2561b008b270bd480ac9cb5e63bc5c45c1f2
sumin-22 <cjstk4_@naver.com>
2022-03-11 11:21:40 +0900

    0311

--- JAVA/src/kr/ac/kopo/day03/exam/ExamMain01.java
+++ JAVA/src/kr/ac/kopo/day03/exam/ExamMain01.java
@@ -3,7 +3,18 @@ package kr.ac.kopo.day03.exam;
 public class ExamMain01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+//		for(int i = 1;i <=10 ; i++) {		//while(true)가 무한루프 for문은 for( ; ;) 형태
+											//일반적으로는 i를 사용
+//		for(int i = 10; i>=1 ; i--) {
+		
+//		for(int i = 0; i <= 9; i++) {
+		
+		for(int i = 5; i <= 14 ; i++) {
+			System.out.println("Hello");
+		}
+		
 
 	}
 
