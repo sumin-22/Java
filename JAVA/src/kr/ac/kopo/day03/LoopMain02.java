bdfc55775e234e6a1b28a5bbfc5b363426971b85
sumin-22 <cjstk4_@naver.com>
2022-03-11 10:59:14 +0900

    0311

--- JAVA/src/kr/ac/kopo/day03/LoopMain02.java
+++ JAVA/src/kr/ac/kopo/day03/LoopMain02.java
@@ -9,6 +9,16 @@ package kr.ac.kopo.day03;
  	for (init; expr; incre) {
  		statement;
  	}
+ 	
+ 	
+ 	
+ 	aaa:	while() {
+ 				switch() {
+ 					case 'A':
+ 					  break:
+ 					  break aaa;
+ 			}
+ 	-> 중첩된 반복문을 한번에 빠져나갈 때 break를 사용할 수 있다. 뒤에 이름을 붙여주어야 한다
  */
 
 public class LoopMain02 {
@@ -28,7 +38,17 @@ public class LoopMain02 {
 		  	++cnt
 		  }
 		  		 */
+		System.out.println("--------------------");
 		
+		
+		cnt = 1;
+		while(true) { //true는 무한루프
+			System.out.println("Hello");
+			++ cnt;
+			
+			if (cnt > 5) {
+				break;    //switch문 이외에 반복문에도 break사용 가능. while 문을 빠져나오게 된다.
+			}
+		}
 	}
-
 }
