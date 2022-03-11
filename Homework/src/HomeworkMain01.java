7000721bb9e24f13862b9434d866e15ad084f642
sumin-22 <cjstk4_@naver.com>
2022-03-11 19:30:01 +0900

    0311

--- Homework/src/HomeworkMain01.java
+++ Homework/src/HomeworkMain01.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class HomeworkMain01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1 - 100 사이의 정수를 입력 : ");
+		int num = sc.nextInt();
+		
+		for(int i =1; i <= num; i++) {
+			System.out.print(i + " ");
+		}
+		
 
 	}
 
---
+++ Homework/src/HomeworkMain02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HomeworkMain02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1 - 100 사이의 정수를 입력 :");
+		int num1 = sc.nextInt();
+		System.out.print("2 - 10 사이의 정수를 입력 :");
+		int num2 = sc.nextInt();
+		
+		System.out.print("<1 ~ " + num1 + " 사이의 " + num2 +"의 배수를 제외한 정수 출력>");
+		
+		
+
+	}
+
+}
