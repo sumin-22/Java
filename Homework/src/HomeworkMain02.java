a86e845b76163c7afd7cd66a41e39d947c9638c8
sumin-22 <cjstk4_@naver.com>
2022-03-11 19:38:02 +0900

    0311

--- Homework/src/HomeworkMain02.java
+++ Homework/src/HomeworkMain02.java
@@ -11,9 +11,12 @@ public class HomeworkMain02 {
 		System.out.print("2 - 10 사이의 정수를 입력 :");
 		int num2 = sc.nextInt();
 		
-		System.out.print("<1 ~ " + num1 + " 사이의 " + num2 +"의 배수를 제외한 정수 출력>");
-		
-		
+		System.out.println("<1 ~ " + num1 + " 사이의 " + num2 +"의 배수를 제외한 정수 출력>");
+		for (int i =1; i <= num1; i++) {
+			if (i % num2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
 
 	}
 
