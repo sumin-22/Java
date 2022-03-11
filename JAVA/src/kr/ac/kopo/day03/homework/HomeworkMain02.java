e43f1b2f2f27f61b16ca830f400139b5e844232f
sumin-22 <cjstk4_@naver.com>
2022-03-11 09:45:15 +0900

    0311

--- JAVA/src/kr/ac/kopo/day03/homework/HomeworkMain02.java
+++ JAVA/src/kr/ac/kopo/day03/homework/HomeworkMain02.java
@@ -4,7 +4,29 @@ public class HomeworkMain02 {
 
 	public static void main(String[] args) {
 		
-
+		int num1 = (int)(Math.random() * 30);
+		int num2 = (int)(Math.random() * 30);
+		int num3 = (int)(Math.random() * 30);
+		int num4 = (int)(Math.random() * 30);
+		
+		
+		System.out.println(num1 + " : " + num2 + " : " + num3 + " : " + num4);
+		
+		
+		int max01 = num1 > num2 ? num1 : num2;
+		int max02 = num3 > num4 ? num3 : num4;
+		int max = max01 > max02 ? max01 : max02;
+		
+		/*int max = num1 > num2 ? num1 : num2;
+		if (max < num3) {
+			max = num3;
+		}
+		if (max < num4) {
+			max = num4;
+		}
+		*/
+		System.out.println("가장 큰 수 : " + max);
+		
 	}
 
 }
