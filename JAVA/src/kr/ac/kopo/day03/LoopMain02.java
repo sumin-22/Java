87ad9575014a6be466c64e7e89853de1fbaaa64c
sumin-22 <cjstk4_@naver.com>
2022-03-11 10:39:33 +0900

    0311

---
+++ JAVA/src/kr/ac/kopo/day03/LoopMain02.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.day03;
+
+
+/*
+ 	while(expr){
+ 		statement;
+ 	}
+ 	
+ 	for (init; expr; incre) {
+ 		statement;
+ 	}
+ 	
+ 	
+ */
+
+public class LoopMain02 {
+
+	public static void main(String[] args) {
+		
+		int cnt = 1;
+		do {
+			System.out.println("Hello");
+			cnt ++;
+		} while(cnt <=5);	
+		
+	}
+
+}
