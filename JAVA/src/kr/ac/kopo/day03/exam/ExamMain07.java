8b6c18f05854515d69be4a7f998b57da151e734f
sumin-22 <cjstk4_@naver.com>
2022-03-11 14:09:25 +0900

    0311

---
+++ JAVA/src/kr/ac/kopo/day03/exam/ExamMain07.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.day03.exam;
+
+public class ExamMain07 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <5; i++) {
+			for (int j = 0; j < 5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ JAVA/src/kr/ac/kopo/day03/exam/ExamMain08.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.day03.exam;
+
+public class ExamMain08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
