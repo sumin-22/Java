4c3faae3dae852e7a3987e6419c1c5646265c510
sumin-22 <cjstk4_@naver.com>
2022-03-11 15:45:50 +0900

    0311

--- JAVA/src/kr/ac/kopo/day03/exam/ExamMain10.java
+++ JAVA/src/kr/ac/kopo/day03/exam/ExamMain10.java
@@ -3,6 +3,22 @@ package kr.ac.kopo.day03.exam;
 public class ExamMain10 {
 
 	public static void main(String[] args) {
+		
+		for(int i =1; i <=9; i++) {
+			if (i <=5) {
+				for (int j=1; j <=i; j++) {
+					System.out.print('*');
+				}
+			} else {
+				for (int j=1; j <= 10-i; j++) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+		
 	
 		for (int i =0; i < 9; i++) {
 			if (i <5) {
