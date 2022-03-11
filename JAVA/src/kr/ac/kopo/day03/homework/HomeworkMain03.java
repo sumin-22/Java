f0a764f4678550cd642ab7d9b8cbc9191c41f674
sumin-22 <cjstk4_@naver.com>
2022-03-11 09:52:14 +0900

    0311

---
+++ JAVA/src/kr/ac/kopo/day03/homework/HomeworkMain03.java
@@ -0,0 +1,20 @@
+package kr.ac.kopo.day03.homework;
+
+public class HomeworkMain03 {
+
+	public static void main(String[] args) {
+		
+		int upperCnt = 'Z' - 'A' +1; //형변환 필요 X
+		int lowerCnt = 'z' - 'a' +1;
+		
+		char ch = 'B';
+		if (ch >= 'A' && ch <= 'Z') {
+			
+		} else if (ch >= 'a' && ch <= 'z') {
+			
+		}
+		
+		
+	}
+
+}
