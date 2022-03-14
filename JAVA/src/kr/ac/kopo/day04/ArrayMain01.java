4746a0d209a13072cc89581441a8acdef68e8f60
sumin-22 <cjstk4_@naver.com>
2022-03-14 15:18:04 +0900

    0314

--- JAVA/src/kr/ac/kopo/day04/ArrayMain01.java
+++ JAVA/src/kr/ac/kopo/day04/ArrayMain01.java
@@ -4,6 +4,11 @@ public class ArrayMain01 {
 
 	public static void main(String[] args) {
 		
+		int [] arr; // 초기화가 되어 있지 않다
+		// 정수 5개를 가지는 배열 생성
+		arr = new int[5];
+		
+		System.out.println("arr : " + arr);  //자동초기화는 new일 때만 가능하다
 
 	}
 
