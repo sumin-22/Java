6d1b47e0487a568cefa183c5348cfb516553741e
sumin-22 <cjstk4_@naver.com>
2022-03-15 14:00:35 +0900

    0315

--- JAVA/src/kr/ac/kopo/day05/ArrayMain08.java
+++ JAVA/src/kr/ac/kopo/day05/ArrayMain08.java
@@ -5,6 +5,13 @@ public class ArrayMain08 {
 	public static void main(String[] args) {
 		String[] strArr = new String[3];
 		
+		strArr[0] = "Hello";
+		strArr[1] = new String("Good");
+		strArr[2] = "Bye Bye -!";
+		
+		for(String str : strArr) {
+			System.out.println(str);
+		}
 
 	}
 
