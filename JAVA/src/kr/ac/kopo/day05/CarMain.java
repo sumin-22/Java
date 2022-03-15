6b5116ab22674fa4d252bec2bc9a4f61fea75874
sumin-22 <cjstk4_@naver.com>
2022-03-15 16:28:34 +0900

    0315

--- JAVA/src/kr/ac/kopo/day05/CarMain.java
+++ JAVA/src/kr/ac/kopo/day05/CarMain.java
@@ -8,9 +8,11 @@ public class CarMain {
 		//new 클래스명();		클래스명 대문자로 써주기!
 		Car c = new Car(); 
 		c.name = "소나타";	// 문자열은 new 쓰지 않아도 됨
+		c.company = "현대";
 		
 		Car c2 = new Car();
 		c2.name = "k9";
+		c.company = "기아";
 		
 		System.out.println("첫번째 자동차 명 : " + c.name);
 		System.out.println("두번째 자동차 명 : " + c2.name);
