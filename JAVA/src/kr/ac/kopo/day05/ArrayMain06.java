70a2959ab12529761a04ad90c5532916e0c3c18c
sumin-22 <cjstk4_@naver.com>
2022-03-15 11:38:51 +0900

    0315

--- JAVA/src/kr/ac/kopo/day05/ArrayMain06.java
+++ JAVA/src/kr/ac/kopo/day05/ArrayMain06.java
@@ -3,7 +3,7 @@ package kr.ac.kopo.day05;
 public class ArrayMain06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] arr = new int [3][4];                      //2차원 배열
 
 	}
 
