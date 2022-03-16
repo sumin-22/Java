7ac2fb4a0515473fb7e54df5594550a3495ae375
sumin-22 <cjstk4_@naver.com>
2022-03-16 11:21:37 +0900

    0316

---
+++ JAVA/src/kr/ac/kopo/day06/MethodMain01.java
@@ -0,0 +1,29 @@
+package kr.ac.kopo.day06;
+/*
+ 	**********
+ 	Hello
+ 	**********
+ 	Hi
+ 	**********
+ 	Bye
+ 	**********
+ 	
+ 	반환형 메소드명([파라미타]) {
+ 		문장;
+ 		문장;
+ 	}
+ */
+
+public class MethodMain01 {
+	
+	void printStar(){	//메소드명은 소문자로 시작 
+		System.out.println("**********");
+	}
+
+	public static void main(String[] args) {
+		
+	
+
+	}
+
+}
