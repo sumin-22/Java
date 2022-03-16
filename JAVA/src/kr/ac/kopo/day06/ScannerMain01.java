87704bdd5464d7615744f3aad10944d98b5966a4
sumin-22 <cjstk4_@naver.com>
2022-03-16 10:18:16 +0900

    0316

--- JAVA/src/kr/ac/kopo/day06/ScannerMain01.java
+++ JAVA/src/kr/ac/kopo/day06/ScannerMain01.java
@@ -7,6 +7,20 @@ public class ScannerMain01 {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		
+		
+		System.out.print("정수 : ");
+		int num = Integer.parseInt(sc.nextLine());
+		System.out.print("문자열 : ");
+		String str = sc.nextLine();		//버퍼에 enter가 남아있음 (정수나 실수형을 입력 받을 때)
+		
+		
+		/*
+		int num = sc.nextInt();
+		sc.nextLine();	//버퍼 비워주기
+		
+		
+		
+		
 		/*
 		System.out.print("첫번째 정수 : ");
 		int num = sc.nextInt();
