819037e250cb32557f61da3f8359d6f06ddf9a37
sumin-22 <cjstk4_@naver.com>
2022-03-16 15:18:34 +0900

    0316

---
+++ JAVA/src/kr/ac/kopo/day06/MethodMain03.java
@@ -0,0 +1,21 @@
+package kr.ac.kopo.day06;
+
+class ExamMethod{
+	
+	int getSum(int a, int b){
+		return a+b;
+	}
+}
+
+public class MethodMain03 {
+
+	public static void main(String[] args) {
+		
+		ExamMethod exam = new ExamMethod();
+		int sum = exam.getSum(10, 20);
+		System.out.println(sum);
+		
+
+	}
+
+}
