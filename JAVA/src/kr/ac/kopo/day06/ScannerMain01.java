be4e9434a54b1cbc7f326ba146d7c3f4eec8c6df
sumin-22 <cjstk4_@naver.com>
2022-03-16 10:09:20 +0900

    0316

---
+++ JAVA/src/kr/ac/kopo/day06/ScannerMain01.java
@@ -0,0 +1,24 @@
+package kr.ac.kopo.day06;
+
+import java.util.Scanner;
+
+public class ScannerMain01 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		/*
+		System.out.print("첫번째 정수 : ");
+		int num = sc.nextInt();
+		
+		System.out.print("두번째 정수 : ");
+		int num2 = sc.nextInt();
+		
+		System.out.print("세번째 정수 : ");
+		int num3 = sc.nextInt();
+		
+		System.out.println("num : "+ num + " num2 : "+ num2 + " num3 : "+ num3);
+		*/
+	}
+
+}
---
+++ JAVA/src/kr/ac/kopo/day06/homework/HomeworkMain01.java
@@ -0,0 +1,32 @@
+package kr.ac.kopo.day06.homework;
+
+import java.util.Scanner;
+
+class Icecream{
+	String name;
+	int prie;
+}
+
+public class HomeworkMain01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("아이스크림 구매 개수 : ");
+		int count = sc.nextInt();
+		
+		Icecream[] iceArr = new Icecream[count];  //주소값을 기억하는 참조배열일 뿐
+		for(int i =0; i < count; i++) {
+			iceArr[i] = new Icecream();		//눈에 보이는 아이스크림 생성
+			
+			System.out.println("*** " + (i+1) + "번째 아이스크림 구매정보 입력 ***");
+			System.out.print("아이스크림명 : ");
+			iceArr[i].name = sc.nextLine();
+			System.out.print("아이스크림 가격 : ");
+			iceArr[i].prie = sc.nextInt();
+		}
+
+	}
+
+}
