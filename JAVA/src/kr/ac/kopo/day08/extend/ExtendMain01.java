2a1b1305cddc12cfba9c3fd50e73b63043559694
sumin-22 <cjstk4_@naver.com>
2022-03-21 15:16:25 +0900

    0321

---
+++ JAVA/src/kr/ac/kopo/day08/extend/ExtendMain01.java
@@ -0,0 +1,26 @@
+package kr.ac.kopo.day08.extend;
+
+public class ExtendMain01 {
+
+	public static void main(String[] args) {
+		Employee e = new Employee("김태형", 3300, "사원");
+		Employee e2 = new Employee("전정국", 3500, "사원");
+		Employee e3 = new Employee("민윤기",4200, "대리");
+		Employee e4 = new Employee("정호석", 4300, "대리");
+		Employee e5 = new Employee("김남준", 5200, "과장");
+		
+		Employee [] empList = {e, e4, e5};
+		Manager01 m = new Manager01("김석진", 9000, "부장", empList);
+		
+		e.info();
+		e2.info();
+		e3.info();
+		e4.info();
+		e5.info();
+		
+		m.info();
+				
+
+	}
+
+}
