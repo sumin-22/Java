53ff824b8466b70aafa31f01e191e3fd57ef008e
sumin-22 <cjstk4_@naver.com>
2022-03-23 11:04:19 +0900

    0323

---
+++ JAVA/src/kr/ac/kopo/day10/inter/LGTV.java
@@ -0,0 +1,5 @@
+package kr.ac.kopo.day10.inter;
+
+public abstract class LGTV implements TV{
+
+}
---
+++ JAVA/src/kr/ac/kopo/day10/inter/TV.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.day10.inter;
+
+public interface TV {
+	int MIN_VOLUME_SIZE = 0;
+	/*public static final*/ int MAX_VOLUME_SIZE = 50;
+	
+	/*public abstract*/ void powerOn();
+	void powerOff();
+	void channelUp();
+	void channelDown();
+	void volumeUp();
+	void volumeDown();
+	void mute();
+	
+}
