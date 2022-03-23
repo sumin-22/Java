a842481f39f26177dd05025c73ea368de38aff61
sumin-22 <cjstk4_@naver.com>
2022-03-23 11:22:31 +0900

    0323

--- JAVA/src/kr/ac/kopo/day10/inter/LGTV.java
+++ JAVA/src/kr/ac/kopo/day10/inter/LGTV.java
@@ -1,5 +1,67 @@
 package kr.ac.kopo.day10.inter;
 
-public abstract class LGTV implements TV{
+public class LGTV implements TV{
+	
+	private boolean power;
+	private int volumeSize;
+	private int channelNo;		// private는 자식들이 볼 수 없음
+	protected static int MAX_CHANNEL_SIZE =100;		//LGTV에서만 보고 싶기 때문
+	
+	
+	public LGTV() {
+		System.out.println("LGTV를 구매했습니다");
+		power = false;
+		volumeSize = 10;
+		channelNo = 3;
+	}
+
+	@Override
+	public void powerOn() {
+		power = true;
+		System.out.println("전원을 켭니다");
+		
+	}
+
+	@Override
+	public void powerOff() {
+		power = false;
+		System.out.println("전원을 끕니다");
+		
+	}
+
+	@Override
+	public void channelUp() {
+		channelNo = ++channelNo % LGTV.MAX_CHANNEL_SIZE;
+		
+	}
+
+	@Override
+	public void channelDown() {
+		channelNo = --channelNo % LGTV.MAX_CHANNEL_SIZE;
+		
+	}
+
+	@Override
+	public void volumeUp() {
+		if(volumeSize < TV.MAX_VOLUME_SIZE) {
+			volumeSize++;					
+		}
+		System.out.println("음량을 높입니다");
+	}
+
+	@Override
+	public void volumeDown() {
+		if(volumeSize > TV.MIN_VOLUME_SIZE) {
+			volumeSize--;
+		}
+		System.out.println("음량을 낮춥니다");
+		
+	}
+
+	@Override
+	public void mute() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- JAVA/src/kr/ac/kopo/day10/inter/TV.java
+++ JAVA/src/kr/ac/kopo/day10/inter/TV.java
@@ -4,6 +4,9 @@ public interface TV {
 	int MIN_VOLUME_SIZE = 0;
 	/*public static final*/ int MAX_VOLUME_SIZE = 50;
 	
+	/*
+	 * 전원을 켜는 기능
+	 */ 
 	/*public abstract*/ void powerOn();
 	void powerOff();
 	void channelUp();
