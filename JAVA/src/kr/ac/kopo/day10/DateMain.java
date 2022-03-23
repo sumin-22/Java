93d0683118dcd10b14d21e501efe0febae999250
sumin-22 <cjstk4_@naver.com>
2022-03-23 14:18:54 +0900

    0323

---
+++ JAVA/src/kr/ac/kopo/day10/DateMain.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.day10;
+
+import java.util.Calendar;
+
+public class DateMain {
+
+	public static void main(String[] args) {
+		
+		Calendar c = Calendar.getInstance();
+		System.out.println(c);
+		
+		
+	
+		
+//		Date d = new Date();
+//		System.out.println(d.toString());
+//		
+//		// 오늘은 2022년 3월 23일 (수)입니다
+//		
+//		int year = d.getYear() + 1900;	// deprecation 됨.
+//		int month =d.getMonth()+ 1;
+//		int date = d.getDate();
+//		int day =d.getDay();
+//		String[] week = {"일","월","화","수","목","금","토"};
+//		
+//		System.out.println("오늘은 "+ year +"년 "+ month + "월 " + date +"일 " + "("+ week[day] + ")" );
+//
+	}
+
+}
