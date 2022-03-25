51d827c59eb2d879a1ba24748572b7e890d3d75d
sumin-22 <cjstk4_@naver.com>
2022-03-25 14:00:47 +0900

    0325

---
+++ JAVA/src/kr/ac/kopo/day12/Member.java
@@ -0,0 +1,35 @@
+package kr.ac.kopo.day12;
+
+public class Member {
+	
+	private String name;
+	private String phone;
+	
+	@Override
+	public String toString() {
+		return "Member [이름 = " + name + ", 전화번호 = " + phone + "]";
+	}
+
+	public Member(String name, String phone) {
+		super();
+		this.name = name;
+		this.phone = phone;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		}
+		Member m =(Member)obj;
+		if(this.name.equals(m.name)) {
+			return false;
+		}
+		if (this.phone.equals(m.phone)) {
+			return false;
+		}
+		return true;
+		
+	}
+
+}
