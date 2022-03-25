698f8f449726c7d6d4363baec4557dfe6abbdca2
sumin-22 <cjstk4_@naver.com>
2022-03-25 14:27:17 +0900

    0325

--- JAVA/src/kr/ac/kopo/day12/Member.java
+++ JAVA/src/kr/ac/kopo/day12/Member.java
@@ -49,7 +49,5 @@ public class Member {
 //		}
 //		return true;
 //	}
-//	
-//	
-//
+
 }
---
+++ JAVA/src/kr/ac/kopo/day12/WrapperMain.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.day12;
+
+/*
+ * 기본자료형을 멤버변수로 가지는 클래스
+ * 논리값, 숫자, 실수, 문자, 문자열을 레퍼런스 타입으로 만들어주는 클래스
+ * 
+ * 기본자료형		Wrapper Class
+ * boolean		Boolean
+ * char			Character
+ * byte			Byte
+ * short		Short
+ * int			Integar
+ * long			Long
+ * float		Float
+ * double		Double
+ * 
+ * class Boolean{
+ * 		private boolean value;
+ */
+
+public class WrapperMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
