46645ce5199bae23cf9b80f1650e70254bf51472
sumin-22 <cjstk4_@naver.com>
2022-03-28 15:38:03 +0900

    0328

--- JAVA/src/kr/ac/kopo/day13/IOMain06.java
+++ JAVA/src/kr/ac/kopo/day13/IOMain06.java
@@ -1,8 +1,14 @@
 package kr.ac.kopo.day13;
 
 public class IOMain06 {
+	
+	public static void write() {
+		Icecream ice = new Icecream("비비빅", 1000);
+	}
 
 	public static void main(String[] args) {
+		
+		write();
 		
 
 	}
