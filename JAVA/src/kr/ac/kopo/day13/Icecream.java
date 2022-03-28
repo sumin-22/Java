4e373b852feedd5607b888c6e7b46e0871a1b644
sumin-22 <cjstk4_@naver.com>
2022-03-28 17:01:33 +0900

    0328

--- JAVA/src/kr/ac/kopo/day13/Icecream.java
+++ JAVA/src/kr/ac/kopo/day13/Icecream.java
@@ -3,36 +3,58 @@ package kr.ac.kopo.day13;
 import java.io.Serializable;
 
 public class Icecream implements Serializable {
+
+	private 			String name;
+	private 			int price;
+	private transient 	String company;	//개체직렬화에서 제외
 	
-	private String name;
-	private int price;
-	
-	
-	@Override
-	public String toString() {
-		return "Icecream [name=" + name + ", price=" + price + "]";
+	public Icecream() {
+		super();
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
+
 	public Icecream(String name, int price) {
 		super();
 		this.name = name;
 		this.price = price;
 	}
-	public Icecream() {
-		super();
-		
-	}
-	
 
+	public Icecream(String name, int price, String company) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.company = company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	@Override
+	public String toString() {
+		return "Icecream [name=" + name + ", price=" + price + ", company=" + company + "]";
+	}
+
+	
+	
+	
 }
