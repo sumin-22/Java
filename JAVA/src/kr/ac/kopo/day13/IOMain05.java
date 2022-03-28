957ca4008ea48e1b2c258477f76227cdeb18100e
sumin-22 <cjstk4_@naver.com>
2022-03-28 15:00:33 +0900

    0328

---
+++ JAVA/src/kr/ac/kopo/day13/IOMain05.java
@@ -0,0 +1,44 @@
+package kr.ac.kopo.day13;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/* 
+ * iotest/IOMain04.java => IOMain04.java.txt복사
+ */
+
+public class IOMain05 {
+	public static void mian(String[] args) {
+		
+		FileReader fr = null;
+		FileWriter fw = null;
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		
+		try {
+			fr = new FileReader("iotest/IOMain04.java");
+			br = new BufferedReader(fr);
+			
+			fw = new FileWriter("iotest/IOMain04.java.txt");
+			bw = new BufferedWriter(fw);
+			
+			while (true) {
+				int c = br.read();
+				if (c != -1) break;
+				bw.write(c);
+			}
+			
+			bw.flush();
+			System.out.println("파일 복사를 완료하였습니다");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
+		
+	}
+
+}
