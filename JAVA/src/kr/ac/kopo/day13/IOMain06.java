f812ff71d98b0cf58c241731548ab1d84616f8c1
sumin-22 <cjstk4_@naver.com>
2022-03-28 15:43:29 +0900

    0328

--- JAVA/src/kr/ac/kopo/day13/IOMain06.java
+++ JAVA/src/kr/ac/kopo/day13/IOMain06.java
@@ -1,9 +1,35 @@
 package kr.ac.kopo.day13;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
+import kr.ac.kopo.util.FileClose;
+
 public class IOMain06 {
 	
 	public static void write() {
 		Icecream ice = new Icecream("비비빅", 1000);
+		
+		FileOutputStream fos = null;
+		DataOutputStream dos = null;
+		
+		try {
+			
+			fos = new FileOutputStream("iotest/icecream.txt");
+			dos = new DataOutputStream(fos);
+			
+			dos.writeUTF(ice.getName());
+			dos.writeInt(ice.getPrice());
+			
+			dos.flush();
+			
+			System.out.println("icecream.txt에 저장완료");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			FileClose.close(dos, fos);
+		}
 	}
 
 	public static void main(String[] args) {
