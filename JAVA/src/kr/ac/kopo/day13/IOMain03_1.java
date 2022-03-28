2512da642eafe6b0e8c84bc5a303a68775e1799e
sumin-22 <cjstk4_@naver.com>
2022-03-28 15:32:02 +0900

    0328

--- JAVA/src/kr/ac/kopo/day13/IOMain03_1.java
+++ JAVA/src/kr/ac/kopo/day13/IOMain03_1.java
@@ -9,10 +9,7 @@ import java.util.Arrays;
 import kr.ac.kopo.util.FileClose;
 
 /*
- * 콘솔모드에서
- * java IOMain03_1 tan.jpg tan2.jpg  입력시
- * 
- * tan.jpg -> tan2.jpg 복사
+ * tan.jpg (입력장치) -> tan2.jpg 복사 (출력장치)
  */
 
 public class IOMain03_1 {
@@ -28,7 +25,6 @@ public class IOMain03_1 {
 		BufferedOutputStream bos = null;
 		
 		try {
-			
 			fis = new FileInputStream(args[0]);
 			bis = new BufferedInputStream(fis);
 			
@@ -42,31 +38,32 @@ public class IOMain03_1 {
 				if(c == -1) break;
 				bos.write(c);
 			}
-			
 			fos.flush();
 			
-		} catch( Exception e) {
-			e.printStackTrace();
+			long end = System.currentTimeMillis();
 			
+			System.out.println("소요시간 : " + (end - start) / 1000. + "초");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
 		} finally {
 			
 			FileClose.close(bis, fis);
 			FileClose.close(bos, fos);
-		
+			
 //			FileClose.close(bis);
 //			FileClose.close(bos);
 //			
 //			FileClose.close(fis);
 //			FileClose.close(fos);
 		}
-		
-		
-		/*finally {
+		/*
+		} finally {
 			
 			if(bis != null) {
 				try {
 					bis.close();
-				} catch (Exception e) {
+				} catch(Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -78,23 +75,21 @@ public class IOMain03_1 {
 					e.printStackTrace();
 				}
 			}
-
+			
 			try {
 				if(fis != null)
 					fis.close();
-			} catch (Exception e) {
+			} catch(Exception e) {
 				e.printStackTrace();
 			}
 			try {
 				if(fos != null)
 					fos.close();
-			} catch(Exception e ) {
+			} catch(Exception e) {
 				e.printStackTrace();
 			}
-		}*/
-		
-		System.out.println("파일 복사가 완료되었습니다");
-
+		}
+	*/	
+		System.out.println("파일복사가 완료되었습니다");
 	}
-
 }
